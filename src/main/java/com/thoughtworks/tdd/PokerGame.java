08950f6fd1bc2fe0e6e2fc3256fe42da94f8fc31
TANGLO2 <1614468806@qq.com>
2019-08-09 18:17:24 +0800

    add:should_return_player1_wins_when_call_compare_given_3H_3D_VS_4H_5C

--- src/main/java/com/thoughtworks/tdd/PokerGame.java
+++ src/main/java/com/thoughtworks/tdd/PokerGame.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.tdd;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class PokerGame {
 
@@ -8,12 +9,12 @@ public class PokerGame {
 
     private static final String POKER_LIST []= new String[]{"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 
-    public static String compareTwoPoker(Poker poker1, Poker poker2) {
+    public static String compareTwoPoker(List<Poker> poker1, List<Poker> poker2) {
         String result = null;
-        if(poker1.getValue().equals(poker2.getValue()))
+        if(poker1.get(0).getValue().equals(poker2.get(0).getValue()))
             result = "equals";
         else {
-            result =  Arrays.asList(POKER_LIST).indexOf(poker1.getValue()) > Arrays.asList(POKER_LIST).indexOf(poker2.getValue()) ? "poker1" : "poker2";
+            result =  Arrays.asList(POKER_LIST).indexOf(poker1.get(0).getValue()) > Arrays.asList(POKER_LIST).indexOf(poker2.get(0).getValue()) ? "poker1" : "poker2";
         }
         return result;
     }
--- src/test/java/com/thoughtworks/tdd/PokerGameTest.java
+++ src/test/java/com/thoughtworks/tdd/PokerGameTest.java
@@ -3,20 +3,46 @@ package com.thoughtworks.tdd;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PokerGameTest {
 
     @Test
-    public  void should_return_3H_when_call_compare_given_3H_2D(){
+    public void should_return_poker1_when_call_compare_given_3H_2D() {
         //given
         Poker poker1 = new Poker("3H");
-        Poker poker2 = new Poker("2D");
+        List<Poker> player1PokerList =new ArrayList<>();
+        player1PokerList.add(poker1);
 
+        Poker poker2 = new Poker("2D");
+        List<Poker> player2PokerList =new ArrayList<>();
+        player2PokerList.add(poker2);
         //when
-        String result = PokerGame.compareTwoPoker(poker1,poker2);
+        String result = PokerGame.compareTwoPoker(player1PokerList, player2PokerList);
 
         //then
-        Assertions.assertEquals("poker1",result);
+        Assertions.assertEquals("poker1", result);
+    }
+
+    @Test
+    public void should_return_player1_wins_when_call_compare_given_3H_3D_VS_4H_5C() {
+        //given
+        List<Poker> player1PokerList =new ArrayList<>();
+        player1PokerList.add(new Poker("3H"));
+        player1PokerList.add(new Poker("3D"));
+
+        List<Poker> player2PokerList =new ArrayList<>();
+        player2PokerList.add(new Poker("4H"));
+        player2PokerList.add(new Poker("5D"));
+
+
+        //when
+        String result = PokerGame.compareTwoPoker(player1PokerList, player2PokerList);
+
+        //then
+        Assertions.assertEquals("player1 wins", result);
     }
 
 
