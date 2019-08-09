5100abb8ec3b3d4d3ef1dab13035f3787fe9201e
TANGLO2 <1614468806@qq.com>
2019-08-09 17:34:43 +0800

    add:should_return_3H_when_call_compare_given_3H_2D

--- src/test/java/com/thoughtworks/tdd/PokerGameTest.java
+++ src/test/java/com/thoughtworks/tdd/PokerGameTest.java
@@ -3,14 +3,20 @@ package com.thoughtworks.tdd;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class PokerGameTest {
+public class PokerGameTest {
 
     @Test
-    public static void should_return_3H_when_call_compare_given_3H_2D(){
-        String result = PokerGame.compareTwoPoker("3H","2D");
-        Assertions.assertEquals("3H",result);
+    public  void should_return_3H_when_call_compare_given_3H_2D(){
+        //given
+        Poker poker1 = new Poker("3H");
+        Poker poker2 = new Poker("2D");
+
+        //when
+        String result = PokerGame.compareTwoPoker(poker1,poker2);
+
+        //then
+        Assertions.assertEquals("poker1",result);
     }
 
 
