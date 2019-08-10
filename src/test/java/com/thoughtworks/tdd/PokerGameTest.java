3e4cd67cf9f1e3150c20d49296649c5bc3cdd4f7
TANGLO2 <1614468806@qq.com>
2019-08-10 17:26:02 +0800

    add:test case should_return_player1_wins_given_3D_3H_3C_10D_10S_VS_6S_9S_JS_KS_AS

--- src/test/java/com/thoughtworks/tdd/PokerGameTest.java
+++ src/test/java/com/thoughtworks/tdd/PokerGameTest.java
@@ -247,7 +247,28 @@ public class PokerGameTest {
         Assertions.assertEquals("player1 wins", result1);
     }
 
+    @Test
+    public void should_return_player1_wins_given_3D_3H_3C_10D_10S_VS_6S_9S_JS_KS_AS(){
+        //given
+        List<Poker> player1PokerList =new ArrayList<>();
+        player1PokerList.add(new Poker("3D"));
+        player1PokerList.add(new Poker("3H"));
+        player1PokerList.add(new Poker("3C"));
+        player1PokerList.add(new Poker("10D"));
+        player1PokerList.add(new Poker("10S"));
 
+        List<Poker> player2PokerList =new ArrayList<>();
+        player2PokerList.add(new Poker("6S"));
+        player2PokerList.add(new Poker("9S"));
+        player2PokerList.add(new Poker("JS"));
+        player2PokerList.add(new Poker("KS"));
+        player2PokerList.add(new Poker("AS"));
+        //when
+        String result1 = PokerGame.compareTwoPlayers(player1PokerList, player2PokerList);
+
+        //then
+        Assertions.assertEquals("player1 wins", result1);
+    }
 
 
 
